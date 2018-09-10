a665fb4a3ebe7a16df70b9148de12dca8dc28442
Chaltin Pagan <chaltinpagan@gmail.com>
2018-09-09 22:59:21 -0400

    heroku version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8000;
+const port = process.env.URL || 8000;
 
 const pwned = require('./routes/pwned');
 const users = require('./routes/users');
@@ -11,11 +11,17 @@ const users = require('./routes/users');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/pwned', pwned);
 app.use('/users', users);
 
 app.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, 'routes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => {
